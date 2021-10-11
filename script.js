61d8e80704d0f7e8766da294a8eb352c8b15e79a
juli741j <juli741j@stud.kea.dk>
2021-10-11 09:25:47 +0200

    gsap

---
+++ script.js
@@ -0,0 +1,34 @@
+gsap.registerPlugin(ScrollTrigger);
+
+// En variabel der henviser til alle sections
+const sections = document.querySelectorAll("section");
+
+// Bruge forEach til at loppe henover den
+
+sections.forEach((section) => {
+  // Lave en henvisning til h2 samt p
+  const h2 = section.querySelectorAll("h2");
+  const p = section.querySelectorAll("p");
+
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: section,
+        start: "top 50%",
+        end: "bottom 75%",
+        scrub: true,
+        pin: true,
+        markers: true,
+      },
+    })
+    .from(h2, {
+      opacity: 0,
+      duration: 1,
+    })
+    .from(p, {
+      opacity: 0,
+      y: 100,
+      stagger: 1,
+      duration: 1,
+    });
+});
